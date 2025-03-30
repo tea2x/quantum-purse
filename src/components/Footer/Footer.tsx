d14f8479a322dea110df9c434f424e55825e5946
ptt <70423834+tea2x@users.noreply.github.com>
2025-03-30 10:20:48 +0700

    Update Footer.tsx
    update footer info

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import styles from "./Footer.module.scss";
 const Footer: React.FC = () => {
   return (
     <div className={styles.footer}>
-      <p>Quantum Purse is a non-custodial wallet for CKB.</p>
+      <p>Quantum Purse is a non-custodial quantum-safe wallet for CKB.</p>
       <p>
         Developed by
         <a href={REPOSITORY_URL} target="_blank" rel="noopener noreferrer">
