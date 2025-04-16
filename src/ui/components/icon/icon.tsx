b50357eab067b41fdddf1eb78db21df950a96441
tea2x <copperheart05@gmail.com>
2025-04-16 16:56:16 +0700

    improve UI

--- src/ui/components/icon/icon.tsx
+++ src/ui/components/icon/icon.tsx
@@ -1,11 +1,13 @@
 import Chip from "./svg/ic-chip.svg?react";
 import Explore from "./svg/ic-explore.svg?react";
 import Hamburger from "./svg/ic-hamburger.svg?react";
+import Connections from "./svg/globe-network-icon.svg?react";
 
 const Icon = {
   Chip,
   Explore,
   Hamburger,
+  Connections
 };
 
 export default Icon;
