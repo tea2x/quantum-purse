d7e7cd5fc5c976d7cf78963f4c1ab705e899f5f6
tea2x <copperheart05@gmail.com>
2025-05-13 11:55:56 +0700

    change paramset order in selection phase

--- src/ui/components/sphincs-param-set/param_selector.tsx
+++ src/ui/components/sphincs-param-set/param_selector.tsx
@@ -9,7 +9,7 @@ const ParamsetSelector: React.FC = () => {
         <span style={{ color: 'var(--gray-01)' }}>
           Parameter set
           <Tooltip
-            title="There are 12 SPHINCS+ parameter sets in total. Sha2_256s is the best choice if you have no reference!"
+            title="There are 12 SPHINCS+ parameter sets in total. Sha2_256s is recommended if you have no reference!"
           >
             <QuestionCircleOutlined style={{ marginLeft: 8 }} />
           </Tooltip>
@@ -22,11 +22,11 @@ const ParamsetSelector: React.FC = () => {
         size="large"
         placeholder="Select a SPHINCS+ variant"
       >
-        <Select.OptGroup label="128-bit Security">
-          <Select.Option value={SphincsVariant.Sha2128S}>Sha2_128s</Select.Option>
-          <Select.Option value={SphincsVariant.Sha2128F}>Sha2_128f</Select.Option>
-          <Select.Option value={SphincsVariant.Shake128S}>Shake_128s</Select.Option>
-          <Select.Option value={SphincsVariant.Shake128F}>Shake_128f</Select.Option>
+        <Select.OptGroup label="256-bit Security">
+          <Select.Option value={SphincsVariant.Sha2256S}>Sha2_256s</Select.Option>
+          <Select.Option value={SphincsVariant.Sha2256F}>Sha2_256f</Select.Option>
+          <Select.Option value={SphincsVariant.Shake256S}>Shake_256s</Select.Option>
+          <Select.Option value={SphincsVariant.Shake256F}>Shake_256f</Select.Option>
         </Select.OptGroup>
         <Select.OptGroup label="192-bit Security">
           <Select.Option value={SphincsVariant.Sha2192S}>Sha2_192s</Select.Option>
@@ -34,11 +34,11 @@ const ParamsetSelector: React.FC = () => {
           <Select.Option value={SphincsVariant.Shake192S}>Shake_192s</Select.Option>
           <Select.Option value={SphincsVariant.Shake192F}>Shake_192f</Select.Option>
         </Select.OptGroup>
-        <Select.OptGroup label="256-bit Security">
-          <Select.Option value={SphincsVariant.Sha2256S}>Sha2_256s</Select.Option>
-          <Select.Option value={SphincsVariant.Sha2256F}>Sha2_256f</Select.Option>
-          <Select.Option value={SphincsVariant.Shake256S}>Shake_256s</Select.Option>
-          <Select.Option value={SphincsVariant.Shake256F}>Shake_256f</Select.Option>
+        <Select.OptGroup label="128-bit Security">
+          <Select.Option value={SphincsVariant.Sha2128S}>Sha2_128s</Select.Option>
+          <Select.Option value={SphincsVariant.Sha2128F}>Sha2_128f</Select.Option>
+          <Select.Option value={SphincsVariant.Shake128S}>Shake_128s</Select.Option>
+          <Select.Option value={SphincsVariant.Shake128F}>Shake_128f</Select.Option>
         </Select.OptGroup>
       </Select>
     </Form.Item>
