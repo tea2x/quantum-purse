7a137162d71a0c9a7637f9e0318d84b0d131cfef
tea2x <copperheart05@gmail.com>
2025-05-15 15:32:04 +0700

    remove gh-pages and related code

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -4,14 +4,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 const common = require("./webpack.common");
 
 let publicPath = "/";
-if (process.env.DEPLOY_TARGET === "gh-pages") {
-  try {
-    const { execSync } = require("child_process");
-    const remoteUrl = execSync("git config --get remote.origin.url").toString().trim();
-    const match = remoteUrl.match(/github\.com[:/][^/]+\/([^/.]+)/);
-    publicPath = publicPath + (match ? match[1] : "") + "/";
-  } catch (error) {}
-} else if (process.env.DEPLOY_TARGET === "electron") {
+if (process.env.DEPLOY_TARGET === "electron") {
   publicPath = "./";
 }
 
