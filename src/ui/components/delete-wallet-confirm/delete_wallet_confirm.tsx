ae1c516a7cf2e68524c7117c88b360733e5a4268
tea2x <copperheart05@gmail.com>
2025-07-01 14:32:30 +0700

    improve ux

---
+++ src/ui/components/delete-wallet-confirm/delete_wallet_confirm.tsx
@@ -0,0 +1,37 @@
+import { Button, Modal } from "antd";
+import React from "react";
+import styles from "./delete_wallet_confirm.module.scss";
+
+interface DeleteWalletModalProps {
+  isOpen: boolean;
+  onOk: () => void;
+  onCancel: () => void;
+}
+
+const DeleteWalletModal: React.FC<DeleteWalletModalProps> = ({
+  isOpen,
+  onOk,
+  onCancel,
+}) => {
+  return (
+    <Modal
+      className={styles.deleteWalletModal}
+      open={isOpen}
+      onCancel={onCancel}
+      title="Delete Wallet"
+      centered
+      footer={
+        <div style={{ display: 'flex', justifyContent: 'center', gap: '10px' }}>
+          <Button onClick={onCancel}>Cancel</Button>
+          <Button type="primary" onClick={onOk}>
+            OK
+          </Button>
+        </div>
+      }
+    >
+      <p>Are you sure you want to delete your wallet?</p>
+    </Modal>
+  );
+};
+
+export default DeleteWalletModal;
