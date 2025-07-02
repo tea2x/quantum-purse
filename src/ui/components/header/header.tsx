b7d38d2c6d6d14bfe9f134a205523a15e9e80ed3
tea2x <copperheart05@gmail.com>
2025-07-02 21:48:55 +0700

    improve header

--- src/ui/components/header/header.tsx
+++ src/ui/components/header/header.tsx
@@ -18,6 +18,7 @@ const Header: React.FC<HeaderProps> = ({ className, ...rest }) => {
   const wallet = useSelector((state: RootState) => state.wallet);
   const { showSidebar, setShowSidebar } = useContext(LayoutCtx);
   const { useBreakpoint } = Grid;
+  const MAX_OUT_BOUNDS = 8; // Maximum number of outbound connections in light client config
   
   const screens = useBreakpoint();
   const location = useLocation();
@@ -39,8 +40,8 @@ const Header: React.FC<HeaderProps> = ({ className, ...rest }) => {
     { name: "Remaining", value: Number(Number(100 - (syncStatus?.syncedStatus || 0)).toFixed(2)) },
   ];
   const peersData = [
-    { name: "Synced", value: Number(Number(syncStatus?.connections).toFixed(2)) || 0 },
-    { name: "Remaining", value: Number(Number(8 - (syncStatus?.connections || 0)).toFixed(2)) },
+    { name: "Connected", value: Number(Number(syncStatus?.connections).toFixed(2)) || 0 },
+    { name: "Waiting", value: Number(Number(MAX_OUT_BOUNDS - (syncStatus?.connections || 0)).toFixed(2)) },
   ];
 
   useEffect(() => {
@@ -51,128 +52,140 @@ const Header: React.FC<HeaderProps> = ({ className, ...rest }) => {
 
   return (
     <header className={cx(styles.header, className)} {...rest}>
-        {isWalletActive && (
-          <>
-            <div className={styles.balanceContainer}>
-              <PieChart width={125} height={125}>
-                <Pie
-                  data={balanceData}
-                  cx="50%"
-                  cy="50%"
-                  innerRadius={20}
-                  outerRadius={60}
-                  dataKey="value"
-                  animationDuration={700}
-                  animationEasing="ease-in-out"
-                  animationBegin={50}
-                >
-                  <Cell fill="#00B27A" />
-                  <Cell fill={noBalance ? "#444" : "#f9652f"} />
-                </Pie>
-                {!noBalance && (
-                  <RechartsTooltip formatter={(value, name) => `${value} CKB`} />
-                )}
-              </PieChart>
-              {screens.md && (
-                <div className={styles.balanceNumbers}>
-                  <span>{wallet.current.name}</span>
-                  <span>Liquid: {balance} CKB</span>
-                  <span>DAO: {locked} CKB</span>
-                </div>
-              )}
+      <>
+        <div className={styles.balancePieChart}>
+          <PieChart width={125} height={125}>
+            <Pie
+              data={balanceData}
+              cx="50%"
+              cy="50%"
+              innerRadius={20}
+              outerRadius={60}
+              startAngle={90}
+              endAngle={-270}
+              dataKey="value"
+              animationDuration={700}
+              animationEasing="ease-in-out"
+              animationBegin={50}
+            >
+              <Cell fill="#00B27A" />
+              <Cell fill={noBalance ? "#444" : "#f9652f"} />
+              <Label
+                value={"CKB"}
+                position="center"
+                fill="var(--gray-01)"
+                style={{ fontSize: '14px', fontWeight: 'bold' }}
+              />
+            </Pie>
+            {!screens.md && !noBalance && (
+              <RechartsTooltip formatter={(value, name) => `${value} CKB`} />
+            )}
+          </PieChart>
+          {screens.md && (
+            <div className={styles.pieChartDetails}>
+              <span>{wallet.current.name}</span>
+              <span>Liquid: {balance} CKB</span>
+              <span>DAO: {locked} CKB</span>
             </div>
+          )}
+        </div>
 
-            <div className={styles.syncContainer}>
-              <PieChart width={125} height={125}>
-                <Pie
-                  data={syncData}
-                  cx="50%"
-                  cy="50%"
-                  innerRadius={45}
-                  outerRadius={60}
-                  startAngle={90}
-                  endAngle={-270}
-                  dataKey="value"
-                  animationDuration={1500}
-                  animationEasing="ease-in-out"
-                  animationBegin={20}
-                >
-                  <Cell fill="#2196F3" />
-                  <Cell fill="#444" />
-                  <Label
-                    value={`${syncStatus && syncStatus.syncedStatus.toFixed(2)}%`}
-                    position="center"
-                    fill="#2196F3"
-                    style={{ fontSize: '16px', fontWeight: 'bold' }}
-                  />
-                </Pie>
-                {/* <RechartsTooltip
-                  formatter={(value, name) => `${value}`}
-                /> */}
-              </PieChart>
-              {screens.md && (
-                <div className={styles.balanceNumbers}>
-                  <span>Tip: {syncStatus && syncStatus.tipBlock.toLocaleString()}</span>
-                  <span>Synced: {syncStatus && syncStatus.syncedBlock.toLocaleString()}</span>
-                  <span>Start: {syncStatus && syncStatus.startBlock.toLocaleString()}</span>
-                </div>
-              )}
+        <div className={styles.syncPieChart}>
+          <PieChart width={125} height={125}>
+            <Pie
+              data={syncData}
+              cx="50%"
+              cy="50%"
+              innerRadius={45}
+              outerRadius={60}
+              startAngle={90}
+              endAngle={-270}
+              dataKey="value"
+              animationDuration={1500}
+              animationEasing="ease-in-out"
+              animationBegin={20}
+            >
+              <Cell fill="#2196F3" />
+              <Cell fill="#444" />
+              <Label
+                value={`SYNC ${syncStatus && syncStatus.syncedStatus.toFixed(0)}%`}
+                position="center"
+                fill="var(--gray-01)"
+                style={{ fontSize: '14px', fontWeight: 'bold' }}
+              />
+            </Pie>
+
+            {!screens.md && (
+              <RechartsTooltip
+                formatter={(value, name) => `${value}`}
+              />
+            )}
+          </PieChart>
+          {screens.md && (
+            <div className={styles.pieChartDetails}>
+              <span>Tip: {syncStatus && syncStatus.tipBlock.toLocaleString()}</span>
+              <span>Synced: {syncStatus && syncStatus.syncedBlock.toLocaleString()}</span>
+              <span>Start: {syncStatus && syncStatus.startBlock.toLocaleString()}</span>
             </div>
+          )}
+        </div>
 
-            <div className={styles.p2pNetworkStatusContainer}>
-              <PieChart width={125} height={125}>
-                <Pie
-                  data={peersData}
-                  cx="50%"
-                  cy="50%"
-                  innerRadius={45}
-                  outerRadius={60}
-                  startAngle={90}
-                  endAngle={-270}
-                  dataKey="value"
-                  animationDuration={200}
-                  animationEasing="ease-in-out"
-                  animationBegin={200}
-                >
-                  <Cell fill="#FFEB3B" />
-                  <Cell fill="#444" />
-                  <Label
-                    value={`${(syncStatus.connections / 8 * 100).toFixed(2)}%`}
-                    position="center"
-                    fill="#2196F3"
-                    style={{ fontSize: '16px', fontWeight: 'bold' }}
-                  />
-                </Pie>
-                {/* <RechartsTooltip
-                  formatter={(value, name) => `${value}`}
-                /> */}
-              </PieChart>
-              {screens.md && (
-                <div className={styles.balanceNumbers}>
-                  <div>
-                  Node Id: {" "}
-                  {syncStatus.nodeId && syncStatus.nodeId !== "NULL" ? (
-                    <Copy value={syncStatus.nodeId} style={{ display: 'inline-block' }}>
-                      <span className={styles.copyable} >{shortenAddress(syncStatus.nodeId, 5, 3)}</span>
-                    </Copy>
-                  ) : (
-                    <span>{syncStatus.nodeId}</span>
-                  )}
-                  </div>
-                  <span>Peers: {parseInt(syncStatus.connections.toString())}</span>
-                </div>
+        <div className={styles.p2pNetworkStatusPieChart}>
+          <PieChart width={125} height={125}>
+            <Pie
+              data={peersData}
+              cx="50%"
+              cy="50%"
+              innerRadius={45}
+              outerRadius={60}
+              startAngle={90}
+              endAngle={-270}
+              dataKey="value"
+              animationDuration={200}
+              animationEasing="ease-in-out"
+              animationBegin={200}
+            >
+              <Cell fill="#FFEB3B" />
+              <Cell fill="#444" />
+              <Label
+                value="P2P"
+                position="center"
+                fill="var(--gray-01)"
+                style={{ fontSize: '14px', fontWeight: 'bold' }}
+              />
+            </Pie>
+
+            {!screens.md && (
+              <RechartsTooltip
+                formatter={(value, name) => `${value}`}
+              />
+            )}
+          </PieChart>
+          {screens.md && (
+            <div className={styles.pieChartDetails}>
+              <div>
+              Id: {" "}
+              {syncStatus.nodeId && syncStatus.nodeId !== "NULL" ? (
+                <Copy value={syncStatus.nodeId} style={{ display: 'inline-block' }}>
+                  <span className={styles.copyable} >{shortenAddress(syncStatus.nodeId, 5, 7)}</span>
+                </Copy>
+              ) : (
+                <span>{syncStatus.nodeId}</span>
               )}
+              </div>
+              <span>Peers Connected: {parseInt(syncStatus.connections.toString())} / {MAX_OUT_BOUNDS}</span>
             </div>
-          </>
-        )}
+          )}
+        </div>
+      </>
 
-        {!screens.md && isWalletActive && (
-          <Button
-            type="text"
-            onClick={() => setShowSidebar(!showSidebar)}
-            icon={<Icon.Hamburger color="var(--white) !important" />}
-          />
-        )}
+      {!screens.md && isWalletActive && (
+        <Button
+          type="text"
+          onClick={() => setShowSidebar(!showSidebar)}
+          icon={<Icon.Hamburger color="var(--white) !important" />}
+        />
+      )}
     </header>
   );
 };
