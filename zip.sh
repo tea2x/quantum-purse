5757e13ec0f68bf0e6fda45119fb8cfb2a669df4
tea2x <copperheart05@gmail.com>
2025-08-08 15:45:21 +0700

    improve zip script

--- zip.sh
+++ zip.sh
@@ -15,11 +15,13 @@ mv build/ zips/linux-x64
 npm run build:app:win:x64
 mv build/ zips/win-x64
 
-zip -r zips/QP-$VERSION-windows-x64-installer.zip zips/win-x64
-zip -r zips/QP-$VERSION-windows-x64-portable.zip zips/win-x64/win-unpacked
-zip zips/QP-$VERSION-macOS-ARM-installer.zip zips/mac-arm/Quantum\ Purse-*-arm64.dmg
-zip -r zips/QP-$VERSION-macOS-ARM-portable.zip zips/mac-arm/mac-arm64/
-zip zips/QP-$VERSION-macOS-x64-installer.zip zips/mac-x64/Quantum\ Purse-*.dmg
-zip -r zips/QP-$VERSION-macOS-x64-portable.zip zips/mac-x64/mac/
-zip zips/QP-$VERSION-linux-x64-installer.zip zips/linux-x64/Quantum\ Purse-*.AppImage
-zip -r zips/QP-$VERSION-linux-x64-portable.zip zips/linux-x64/linux-unpacked/
+cd zips/
+
+zip -r Quantum-Purse-$VERSION-windows-x64-installer.zip win-x64
+zip -r Quantum-Purse-$VERSION-windows-x64-portable.zip win-x64/win-unpacked
+zip Quantum-Purse-$VERSION-macOS-ARM-installer.zip mac-arm/Quantum\ Purse-*-arm64.dmg
+zip -r Quantum-Purse-$VERSION-macOS-ARM-portable.zip mac-arm/mac-arm64/
+zip Quantum-Purse-$VERSION-macOS-x64-installer.zip mac-x64/Quantum\ Purse-*.dmg
+zip -r Quantum-Purse-$VERSION-macOS-x64-portable.zip mac-x64/mac/
+zip Quantum-Purse-$VERSION-linux-x64-installer.zip linux-x64/Quantum\ Purse-*.AppImage
+zip -r Quantum-Purse-$VERSION-linux-x64-portable.zip linux-x64/linux-unpacked/
