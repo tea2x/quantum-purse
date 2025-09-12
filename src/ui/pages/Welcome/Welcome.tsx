4a3ecbff19aa68c03a5935f01c085a8291872cfa
tea2x <copperheart05@gmail.com>
2025-09-12 10:52:30 +0700

    update welcome page

--- src/ui/pages/Welcome/Welcome.tsx
+++ src/ui/pages/Welcome/Welcome.tsx
@@ -8,31 +8,25 @@ const Welcome: React.FC = () => {
 
   return (
     <section className={styles.welcome}>
-      {/* Top Section */}
       <div className={styles.header}>
         <h1>WELCOME</h1>
       </div>
 
-      {/* Middle Section */}
-      <div className={styles.middle}>
-        <p className={styles.infoText}>
-          Quantum Purse is a lightweight client wallet, post-quantum hardened and powered by CKB. Learn more about how to get started with your product, receive new
-          feature updates and more.
-        </p>
+      <p className={styles.infoText}>
+        Quantum Purse is a lightweight client wallet, post-quantum hardened and powered by CKB blockchain. Learn more about how to get started with your product, receive new feature updates and more.
+      </p>
 
-        <Input
-          className={styles.emailInput}
-          placeholder="e-mail address"
-          bordered={false}
-        />
+      <Input
+        className={styles.emailInput}
+        placeholder="e-mail address"
+        bordered={false}
+      />
 
-        <p className={styles.disclaimer}>
-          Your email address is safe with us. We’ll only use it to share
-          important product updates — no spam, ever.
-        </p>
-      </div>
+      <p className={styles.disclaimer}>
+        Your email address is safe with us. We’ll only use it to share
+        important product updates — no spam, ever.
+      </p>
 
-      {/* Bottom Section */}
       <div className={styles.footer}>
         <Button
           onClick={() => navigate(ROUTES.CREATE_WALLET, { replace: true })}
